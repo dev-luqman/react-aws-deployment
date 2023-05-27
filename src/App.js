6f4f9d58b72f4445a272799d01b01cbd3b80d745
balogun Lukman <lukheebalo@gmail.com>
2023-05-27 17:55:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -9,14 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        APP Version {process.env.REACT_APP_BASE_URL}
       </header>
     </div>
   );
